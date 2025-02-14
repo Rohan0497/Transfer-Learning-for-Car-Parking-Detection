1b3a67e9a4691e58df6c3a46c9c4b5dc327dcf66
Rohan0497 <78637250+Rohan0497@users.noreply.github.com>
2025-02-14 20:51:09 +0000

    inference and evaluation

--- src/components/evaluation.py
+++ src/components/evaluation.py
@@ -2,13 +2,12 @@ import torch
 from src.components.model import create_model
 from src.config.configuration import load_config
 
-def run_inference(image_path):
+def evaluate():
     config = load_config()
     model = create_model(num_classes=2)
     model.load_state_dict(torch.load("outputs/trained_model.pth"))
     model.eval()
-    print(f"Inference completed for {image_path}.")
+    print("Model evaluation completed.")
 
 if __name__ == "__main__":
-    test_image = "test.jpg"
-    run_inference(test_image)
+    evaluate()
