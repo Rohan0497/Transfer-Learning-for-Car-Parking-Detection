0b324d99eabf1c4b72a5647f222c2acd9413c1b2
Rohan0497 <78637250+Rohan0497@users.noreply.github.com>
2025-02-14 21:18:25 +0000

    utils

---
+++ src/utils/common.py
@@ -0,0 +1,77 @@
+import os
+from box.exceptions import BoxValueError
+import yaml
+from src import logger
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+import base64
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """Reads YAML file and returns its contents as a ConfigBox object"""
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"YAML file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("YAML file is empty")
+    except Exception as e:
+        raise e
+
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """Create directories if they do not exist"""
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"Created directory at: {path}")
+
+@ensure_annotations
+def save_json(path: Path, data: dict):
+    """Save JSON data to a file"""
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+    logger.info(f"JSON file saved at: {path}")
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    """Load JSON file as a ConfigBox object"""
+    with open(path) as f:
+        content = json.load(f)
+    logger.info(f"JSON file loaded successfully from: {path}")
+    return ConfigBox(content)
+
+@ensure_annotations
+def save_bin(data: Any, path: Path):
+    """Save binary file using joblib"""
+    joblib.dump(value=data, filename=path)
+    logger.info(f"Binary file saved at: {path}")
+
+@ensure_annotations
+def load_bin(path: Path) -> Any:
+    """Load binary file using joblib"""
+    data = joblib.load(path)
+    logger.info(f"Binary file loaded from: {path}")
+    return data
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    """Get file size in KB"""
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~ {size_in_kb} KB"
+
+def decodeImage(imgstring, fileName):
+    """Decode base64 image and save it to file"""
+    imgdata = base64.b64decode(imgstring)
+    with open(fileName, 'wb') as f:
+        f.write(imgdata)
+
+def encodeImageIntoBase64(croppedImagePath):
+    """Encode image file into base64 string"""
+    with open(croppedImagePath, "rb") as f:
+        return base64.b64encode(f.read())
